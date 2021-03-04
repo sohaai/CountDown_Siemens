4a47c81e507ae5155f56103cfdcd45a334580ac3
sohaai <54282266+sohaai@users.noreply.github.com>
2021-03-04 15:11:16 +0500

    initial commit

---
+++ js/config.js
@@ -0,0 +1,15 @@
+﻿var myDomainId = "Your domain (optional)";
+
+var eventDate = "Feb 25, 2021 12:00:01";
+//title lenght:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" if showDate=false (32 chars).
+//title lenght:"aaaaaaaaaaaaaaaaa" if showDate=true (17 chars).
+var eventTitle = "Siemens";
+// if showDate is false then the Subtitle can be displayed on 2 rows, each row having the length below.
+//subtitle length:"Siemens" (57 chars).
+var eventSubtitle = "New Organization Day 1";
+var dayLabel = "Days";
+var hourLabel = "Hours";
+var minuteLabel = "Minutes";
+var secondLabel = "Seconds";
+// set to true if you want the Date to be displyed
+var showDate = true;
---
+++ js/main.js
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+p {
+  text-align: center;
+  font-size: 60px;
+  margin-top: 0px;
+}
+</style>
+</head>
+<body>
+
+<p id="demo"></p> 
+  
+<script> 
+  
+var deadline = new Date("march 10, 2021 15:37:25").getTime(); 
+  
+var x = setInterval(function() { 
+  
+var now = new Date().getTime(); 
+var t = deadline - now; 
+var days = Math.floor(t / (1000 * 60 * 60 * 24)); 
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
+var seconds = Math.floor((t % (1000 * 60)) / 1000); 
+document.getElementById("day").innerHTML =days ; 
+document.getElementById("hour").innerHTML =hours; 
+document.getElementById("minute").innerHTML = minutes;  
+document.getElementById("second").innerHTML =seconds;  
+if (t < 0) { 
+        clearInterval(x); 
+        document.getElementById("demo").innerHTML = "TIME UP"; 
+        document.getElementById("day").innerHTML ='0'; 
+        document.getElementById("hour").innerHTML ='0'; 
+        document.getElementById("minute").innerHTML ='0' ;  
+        document.getElementById("second").innerHTML = '0'; } 
+}, 1000); 
+</script> 
+
+</body>
+</html>
